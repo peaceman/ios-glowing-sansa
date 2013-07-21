0ba538ca9178052d86ad2f390f12badbd61fb3b9
peaceman <nixalio@gmail.com>
2013-07-21 20:26:16 +0200

    Initial Commit

---
+++ LodgeManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LodgeManager
+//
+//  Created by Nico Nägele on 7/21/13.
+//  Copyright (c) 2013 n2305. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LodgeManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LodgeManager
+//
+//  Created by Nico Nägele on 7/21/13.
+//  Copyright (c) 2013 n2305. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
